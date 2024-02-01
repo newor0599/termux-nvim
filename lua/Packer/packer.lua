60b74420736f76c7f785d4745c956091ee9fb2f0
newor0599 <rowen778623@gmail.com>
2024-02-01 12:32:39 +0800

    Added Live server plugin

--- lua/Packer/packer.lua
+++ lua/Packer/packer.lua
@@ -28,5 +28,6 @@ return require('packer').startup(function(use)
 	use {'hrsh7th/cmp-vsnip'}
 	use {'hrsh7th/vim-vsnip'}
 	use {'rafamadriz/friendly-snippets'}
+	use "barrett-ruth/live-server.nvim"
 end)
 
